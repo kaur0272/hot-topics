d518a6abb301526005a9a9d88d715c7009326dc1
Ramandeep Kaur <35570044+kaur0272@users.noreply.github.com>
2018-04-27 15:20:10 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,43 +1,46 @@
-const $container = document.querySelector(".main .content");
+var $container = document.querySelector(".main");
+var $links = document.querySelectorAll("nav ul li a");
 
-const $links = document.querySelectorAll("nav ul li a");
-
-const contents = {};
+var contents = {};
 
 fetch("./partials/home.html")
-    .then(function (response) {
+  .then(function (response) {
+    return response.text();
+  })
+  .then(function (data) {
+    $container.innerHTML = data;
+  })
+
+var storeContents = function (urlVal) {
+
+  if (!contents[urlVal]) {
+
+    fetch(urlVal)
+      .then(function (response) {
         return response.text();
-    })
-    .then(function (data) {
-        $container.innerHTML = data;
-    });
-
-const storeContents = function (urlVal) {
-
-    if (!contents[urlVal]) {
-
-        fetch(urlVal)
-            .then(function (response) {
-                return response.text();
-            })
-            .then(function (data) {
-                contents[urlVal] = data;
-                $container.innerHTML = contents[urlVal];
-            });
-    } else {
+      })
+      .then(function (data) {
+        contents[urlVal] = data;
         $container.innerHTML = contents[urlVal];
-    }
+      });
+  } else {
+    $container.innerHTML = contents[urlVal];
+  }
 };
 
+
 const handleClick = function (e) {
-    e.preventDefault();
 
-    let key = e.target.href;
+  e.preventDefault();
 
-    storeContents(key);
+  let key = e.target.href;
+
+  storeContents(key);
+
 };
 
 for (let i = 0; i < $links.length; i++) {
 
-    $links[i].addEventListener("click", handleClick);
+  $links[i].addEventListener("click", handleClick);
+
 }
